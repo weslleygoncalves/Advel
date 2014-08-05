685f55259372d665e910a5917939bd744906046c
Weslley <Weslley@W>
2014-08-05 20:18:00 -0300

    Protótipos das telas

---
+++ src/projeto_integrador/Projeto_Integrador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto_integrador;
+
+import Telas.TelaPrincipal;
+
+/**
+ *
+ * @author Roni Vitor
+ */
+public class Projeto_Integrador {
+
+    
+    public static void main(String[] args) {
+        System.out.println ("Testando o sistema");
+        new Telas().setVisible(true);
+    }
+    
+}
---
+++ src/projeto_integrador/Telas.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto_integrador;
+
+/**
+ *
+ * @author Roni Vitor
+ */
+public class Telas {
+    
+}
